138bb6f40128837327622e754b1094d33c0ff0f3
Bruno Souza <bruno.arubesu@gmail.com>
2019-12-24 21:47:12 -0300

    Add Pokedex, Pokemon class

---
+++ Multithreading/Pokemon.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace Multithreading
+{
+	public class Pokemon
+	{
+		public int Id { get; set; }
+		public PokemonName Name { get; set; }
+		public PokemonBase Base { get; set; }
+	}
+
+	public class PokemonName
+	{
+		public string English { get; set; }
+		public string Japanese { get; set; }
+		public string Chinese { get; set; }
+		public string French { get; set; }
+	}
+
+	public class PokemonBase
+	{
+		public int HP { get; set; }
+		public int Attack { get; set; }
+		public int Defense { get; set; }
+		[JsonProperty("Sp. Attack")]
+		public int SPAttack { get; set; }
+		[JsonProperty("Sp. Defense")]
+		public int SPDefense { get; set; }
+		public int Speed { get; set; }
+	}
+}
